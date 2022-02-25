10419e4646c6c274ef06f46c208127c4b9c72b47
Shah rahman <toshah.rahman@gmail.com>
2022-02-26 00:06:33 +0200

    website visit, assertions and click event

--- cypress/integration/examples/first.spec.js
+++ cypress/integration/examples/first.spec.js
@@ -1,11 +1,15 @@
-describe('First test', () => {
-    it('visit website - google',()=>{
-        cy.visit('https://www.google.com')
+describe('website page test', () => {
+    it('visit website - bookstore',()=>{
+        cy.visit('https://books.toscrape.com/')
     })
     it('assertion- check url',()=>{
-        cy.url().should('include', 'google')
+        cy.url().should('include', 'toscrape')
     })
     it('check element visible',()=>{
         cy.get('h1').should('be.visible')
     })
+    it('click an element',()=>{
+        cy.get('a').contains('Travel').click()
+    })
+
 })
