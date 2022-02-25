7a8600b71434915d727ded7d5b96795279b58b17
Shah rahman <toshah.rahman@gmail.com>
2022-02-25 23:07:34 +0200

    script update for visit a website

--- cypress/integration/examples/first.spec.js
+++ cypress/integration/examples/first.spec.js
@@ -2,4 +2,7 @@ describe('First test', () => {
     it('First test assert',()=>{
         expect(true).to.equal(true)
     })
+    it('visit website - google',()=>{
+        cy.visit('https://www.google.com')
+    })
 })
