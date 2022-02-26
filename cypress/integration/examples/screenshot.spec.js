1f0e4c0b7cfb5e27b798928c7df84a6fbfec4e39
Shah rahman <toshah.rahman@gmail.com>
2022-02-26 14:21:29 +0200

    screenshot full page

--- cypress/integration/examples/screenshot.spec.js
+++ cypress/integration/examples/screenshot.spec.js
@@ -1,6 +1,7 @@
 describe('Screenshot tests', () => {
     it('Full page screenshot', () => {
-        
+        cy.visit('https://books.toscrape.com/index.html')
+        cy.screenshot({capture:'fullPage'})
     })
     it('Single element screenshot', () => {
         
