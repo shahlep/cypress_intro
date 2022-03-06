a2b50bf58acaa9a279bae037cfd38bdedc5ccf8e
Shah rahman <toshah.rahman@gmail.com>
2022-03-06 08:49:49 +0200

    commands fun step2

--- cypress/integration/examples/commands.spec.js
+++ cypress/integration/examples/commands.spec.js
@@ -3,4 +3,8 @@ describe('commands fun', () => {
       cy.visit('cypress/index.html')
       cy.url().should('contain','index.html')  
     })
+    it('try with title command', () => {
+      cy.visit('cypress/index.html')
+      cy.title().should('eq','Cypress Tutorials')  
+    })
 })
