9e7df7ae05668fa4d7ab493ee33822b2a48273ca
Shah rahman <toshah.rahman@gmail.com>
2022-03-06 09:04:05 +0200

    commands fun step3-links update

--- cypress/integration/examples/commands.spec.js
+++ cypress/integration/examples/commands.spec.js
@@ -1,14 +1,17 @@
 describe('commands fun', () => {
     it('try with url command', () => {
+        //https://docs.cypress.io/api/commands/url
       cy.visit('cypress/index.html')
       cy.url().should('contain','index.html')  
     })
     it('try with title command', () => {
+        //https://docs.cypress.io/api/commands/title
       cy.visit('cypress/index.html')
       cy.title().should('eq','Cypress Tutorials')  
     })
 
     it('try with go command', () => {
+        //https://docs.cypress.io/api/commands/go#Arguments
       cy.visit('cypress/index.html')
       cy.contains('About').click()
       cy.go('back')
